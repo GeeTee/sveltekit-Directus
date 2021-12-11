771d7829fb7cbdb5d82ee01d1ebf116eb997bc3c
Gille Testard <g.testard@galites.net>
2021-12-11 10:19:02 +0100

    install sass ok

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,5 +1,24 @@
-<h1>Kit Directus</h1>
+<div class="test">
+    <h1>Kit Directus</h1>
 <ul>
     <li>Install seveltkit</li>
     <li>Install directus</li>
 </ul>
+</div>
+
+<style lang="scss">
+    .test {
+        background-color: grey;
+        h1 {
+            color: greenyellow
+        }
+        ul {
+            list-style-type: none;
+            > li {
+                &:not(:first-child) {
+                    margin-top: 1rem;
+                }
+            }
+        }
+    }
+</style>
