212cd6227f9fba2dde74560b535adc293540539a
Gille Testard <g.testard@galites.net>
2021-12-11 10:37:08 +0100

    add div.notification to check bulma. remove sveltekit-bulma

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,14 +1,18 @@
 <div class="test">
     <h1>Kit Directus</h1>
+    <h2 class="subtitle">Test Bulma</h2>
 <ul>
     <li>Install seveltkit</li>
     <li>Install directus</li>
+    <li>Install Sass</li>
 </ul>
 </div>
+<div class="notification is-info">Bulma</div>
 
 <style lang="scss">
     .test {
         background-color: grey;
+        padding-bottom: 1rem;
         h1 {
             color: greenyellow
         }
